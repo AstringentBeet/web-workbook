b8e27a4ed89fdbcfcad5b25608ec43960f4a752b
Alexander Effanga <alexandereffanga@Alexanders-MacBook-Air.local>
2018-03-14 20:16:45 -0500

    upadated shit

--- 06week/javascripting/scope.js
+++ 06week/javascripting/scope.js
@@ -1,18 +1,18 @@
 var a = 1, b = 2, c = 3;
+//lint was being weird af analyzing this in regard to the spaces.
+(function firstFunction(){
+   var b = 5, c = 6;
 
-     (function firstFunction(){
-         var b = 5, c = 6;
+   (function secondFunction(){
+       var b = 8;
 
-         (function secondFunction(){
-             var b = 8;
+       (function thirdFunction(){
+           var a = 7, c = 9;
 
-             (function thirdFunction(){
-                 var a = 7, c = 9;
-
-                 (function fourthFunction(){
-                     var a = 1, b = 8, c = 6;
-                     console.log("a: "+a+", b: "+b+", c: "+c);
-                 })();
-             })();
-         })();
-     })();
+           (function fourthFunction(){
+               var a = 1, b = 8, c = 6;
+               console.log("a: "+a+", b: "+b+", c: "+c);
+           })();
+       })();
+   })();
+})();
